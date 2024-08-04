8607031f236616df6feb35321c89228108d82c70
Gustavo Adolfo Godoy Barrera <99930726+gustavo669@users.noreply.github.com>
2024-08-03 18:46:56 -0600

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import Vehiculos.Coche;
+import Vehiculos.Moto;
+import Vehiculos.Vehiculo;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehiculo coche = new Coche("Toyota", "Corolla", 2020, 4);
+        Vehiculo moto = new Moto("Harley Davidson", "Sportster", 2018, true);
+
+        System.out.println("Detalles del Coche:");
+        coche.mostrarDetalles();
+
+        System.out.println("\nDetalles de la Moto:");
+        moto.mostrarDetalles();
+    }
 }
