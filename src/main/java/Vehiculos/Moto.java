8f5a3872c25d2c19607014ef154981a49ba0f9de
Gustavo Adolfo Godoy Barrera <99930726+gustavo669@users.noreply.github.com>
2024-08-03 18:49:08 -0600

    Add files via upload

--- src/main/java/Vehiculos/Moto.java
+++ src/main/java/Vehiculos/Moto.java
@@ -1,2 +1,15 @@
-package Vehiculos;public class Moto {
+package Vehiculos;
+public class Moto extends Vehiculo {
+    private final boolean tieneSidecar;
+
+    public Moto(String marca, String modelo, int ano, boolean tieneSidecar) {
+        super(marca, modelo, ano);
+        this.tieneSidecar = tieneSidecar;
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles();
+        System.out.println("Tiene sidecar: " + (tieneSidecar ? "Sí" : "No"));
+    }
 }
